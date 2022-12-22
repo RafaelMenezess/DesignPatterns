b0567dcb147a5a5517c455356d6eaed34d53d440
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-22 19:36:33 -0300

    Add project files.

---
+++ DesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCaculo(Orcamento orcamento, Imposto imposto)
+        {
+            double calculoImposto = imposto.Calcula(orcamento);
+            Console.WriteLine(calculoImposto);
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Imposto iss = new ISS();
+            Imposto icms = new ICMS();
+
+            Orcamento orcamento = new Orcamento(500.0);
+
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+
+            calculador.RealizaCaculo(orcamento, icms);
+            calculador.RealizaCaculo(orcamento, iss);
+
+            Console.ReadKey();
+        }
+    }
+}
